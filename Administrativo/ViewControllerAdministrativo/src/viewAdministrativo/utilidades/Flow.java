32f9012a035cb0bd653203fbb2fc24cc8f2849ec
Omar Velez <omargo33@gmail.com>
2023-04-16 21:03:12 -0500

    Up  [13a-arreglos]

--- Administrativo/ViewControllerAdministrativo/src/viewAdministrativo/utilidades/Flow.java
+++ Administrativo/ViewControllerAdministrativo/src/viewAdministrativo/utilidades/Flow.java
@@ -1,8 +1,9 @@
   package viewAdministrativo.utilidades;
 
-
-public class Flow
-  {
+/**
+ * Metodo para modificar el flow predefinido.
+ */
+public class Flow{
     public static final String BUNDLE = "viewAdministrativo.ViewControllerAdministrativoBundle";
     public static final String ACCION_ROLLBACK = "Rollback";
     public static final String ACCION_COMMIT = "Commit";
@@ -11,9 +12,3 @@ public class Flow
     public static final String FLOW_INICIO = "Inicio";
     public static final String FLOW_NULL = null;
   }
-
-
-/* Location:              /home/omarv/Documentos/jdeveloper/mywork122140/dup/Administrativo-001/Administrativo-0013319179167455158583.war!/WEB-INF/classes/viewAdministrativo/utilidades/Flow.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.2
- */
